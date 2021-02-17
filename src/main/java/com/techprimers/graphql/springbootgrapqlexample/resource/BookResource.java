f284aeedf272d61df5611fb762795dd8c4c0a7fe
GovindKoduri <govindk.destiny@gmail.com>
2021-02-18 00:00:50 +0530

    Add files via upload

---
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/resource/BookResource.java
@@ -0,0 +1,42 @@
+package com.techprimers.graphql.springbootgrapqlexample.resource;
+
+import com.techprimers.graphql.springbootgrapqlexample.model.Book;
+import com.techprimers.graphql.springbootgrapqlexample.service.BookService;
+import com.techprimers.graphql.springbootgrapqlexample.service.GraphQLService;
+import graphql.ExecutionResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/rest/books")
+@RestController
+@Slf4j
+public class BookResource {
+
+    @Autowired
+    GraphQLService graphQLService;
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/graphql")
+    public ResponseEntity<Object> getAllBooks(@RequestBody String query) {
+        log.info("GraphQL query: {}", query);
+        ExecutionResult execute = graphQLService.getGraphQL().execute(query);
+
+        return new ResponseEntity<>(execute, HttpStatus.OK);
+    }
+
+    @PostMapping("/rest")
+    public ResponseEntity<Object> saveBook(@RequestBody Book book) {
+        log.info("Save Book request : {}", book);
+        Book bookResponse = bookService.saveBook(book);
+        log.info("Book response : {}", bookResponse);
+        return new ResponseEntity<>(bookResponse, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/service/BookService.java
@@ -0,0 +1,17 @@
+package com.techprimers.graphql.springbootgrapqlexample.service;
+
+import com.techprimers.graphql.springbootgrapqlexample.model.Book;
+import com.techprimers.graphql.springbootgrapqlexample.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+}
